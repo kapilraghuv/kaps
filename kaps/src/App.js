a8046a5e6d70b1408250b4631b4e67aa276f96e1
kapil <kapilraghuv@gmail.com>
2023-03-29 21:16:14 +0530

    mui

---
+++ kaps/src/App.js
@@ -0,0 +1,20 @@
+import Mybtn from "./Mybtn";
+
+const hcolor = {
+    color:"red",
+  textAlign:"center",
+  fontFamily:"Questrial",
+  fontSize:"100px",    
+}
+
+function App(){
+    return (
+        <>
+            <h2 style={hcolor}>Hello App Data Here</h2>
+            <p>dklfjdkljf</p>
+            <Mybtn />
+        </>
+    )
+}
+
+export default App;
